b45ae15ed63bba46360ec01c7614a018207997cc
erdemgeleri <erdem_geleri@hotmail.com>
2026-02-01 20:18:38 +0300

    Customer page init for admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -8,4 +8,13 @@ class Admin extends MY_Controller{
     public function dashboard() {
         $this->load->view('admin/dashboard');
     }
+    public function customers() {
+        $this->load->view('admin/customer');
+    }
+    public function add_customer(){
+        $this->load->view('admin/add_customer');
+    }
+    public function add_customer_post(){
+        // Müşteri ekleme işlemleri burada yapılacak
+    }
 }
---
+++ application/models/Customer_model.php
@@ -0,0 +1,7 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Customer_model extends CI_model{
+    protected $table = 'customers';
+    
+}
